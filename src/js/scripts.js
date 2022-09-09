3f4a5d71ceac2ccb8436d7ec51d48421598d00c5
EvgeniyLyapunov <lyapunovcs@gmail.com>
2022-09-09 16:14:16 +0300

    layout and styles dropdown and dropdown js script

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,22 +1,31 @@
 const swiper = new Swiper('.swiper', {
-  // autoplay: {
-  //   delay: 5000,
-  // },
+  autoplay: {
+    delay: 5000,
+  },
   effect: 'fade',
   fadeEffect: {
     crossFade: true
   }
 });
 
-// new SimpleBar(document.getElementById('#simplebar'));
-
+// Drop down
 const catalogSections = document.querySelectorAll(".header__catalog-section");
 
+function openCloseDropDown() {
+  catalogSections.forEach((el) => {
+    if(el == this) {
+      el.children[1].classList.toggle("drop-down_active");
+      el.classList.toggle("header__catalog-section_open");
+    }
+    else {
+      el.children[1].classList.remove("drop-down_active");
+      el.classList.remove("header__catalog-section_open");
+    }
+  });
+};
+
 catalogSections.forEach((el) => {
-  el.addEventListener("click", () => {
-    const dropDown = el.children;
-    dropDown[1].classList.add("drop-down_active");
-  })
+  el.addEventListener("click", openCloseDropDown);
 });
 
 new SimpleBar(document.getElementById('#simplebar'));
